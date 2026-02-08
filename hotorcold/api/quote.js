a2efa46c01514584e61677e57e8b8e4599ac55d2
Brandon <bzuruce@gmail.com>
2026-02-08 15:35:44 -0800

    some stuff

--- hotorcold/api/quote.js
+++ hotorcold/api/quote.js
@@ -1,10 +1,6 @@
 export default function handler(request, response) {
   const quotes = [
-    "The only way to do great work is to love what you do. - Steve Jobs",
-    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
-    "Stay hungry, stay foolish. - Steve Jobs",
-    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-    "It does not matter how slowly you go as long as you do not stop. - Confucius"
+    "hello"
   ];
 
   const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
